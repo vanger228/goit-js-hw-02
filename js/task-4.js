139f8d7e5acd68c7cc2c243ce582ca4308cbd356
vanger228 <112012329+vanger228@users.noreply.github.com>
2023-12-16 20:15:07 +0200

    end task

---
+++ js/task-4.js
@@ -0,0 +1,50 @@
+// function getShippingCost(country) {
+//   // let price;
+//   switch (country) {
+//     case 'China':
+//       return `"Shipping to ${country} will cost 100 credits"`;
+//     case 'Chile':
+//       return `"Shipping to ${country} will cost <price> credits"`;
+//     case 'Australia':
+//       return `"Shipping to ${country} will cost 170 credits"`;
+//     case 'Jamaica':
+//       return `"Shipping to ${country} will cost 120 credits"`;
+//     default:
+//       return 'Sorry, there is no delivery to your country';
+//   }
+// }
+
+function getShippingCost(country) {
+  let price;
+  let message;
+
+  switch (country) {
+    case 'China':
+      price = 100;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case 'Chile':
+      price = 250;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case 'Australia':
+      price = 170;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case 'Jamaica':
+      price = 120;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    default:
+      return 'Sorry, there is no delivery to your country';
+  }
+
+  return message;
+}
+
+console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
