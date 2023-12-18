fe3d1441e806103e8c3e43fd62d89da181606fee
vanger228 <112012329+vanger228@users.noreply.github.com>
2023-12-18 22:24:24 +0200

    edited3

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,7 @@
 function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else {
-    return message.slice(0, maxLength) + '...';
-  }
+  return message.length <= maxLength
+    ? message
+    : message.slice(0, maxLength) + '...';
 }
 
 console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
